aa9199116b1f2003d257f2fc6d6c2b2ca0fd7dd0
Jeff Trull <jeff.trull@ciere.com>
2018-05-17 16:09:21 -0700

    Model long-latency task as multiply for clarity

--- callbacks.cpp
+++ callbacks.cpp
@@ -4,13 +4,34 @@
 #include <iostream>
 #include "run_queue.hpp"
 
+// a representative expensive/high latency calculation
+// that delivers results with a callback
+template<typename Callback>
+void multiply(int a, int b, Callback cb) {
+    int result = a * b;
+    cb(result);
+}
+
 int main() {
     run_queue work;
+
+    // use the work queue to calculate 2*3+4 in a really dumb way
+    // we launch a task to calculate 2*3, with a callback that adds 4
+    // this models a long-running task (the multiply) with a finishing callback
+    // which I hope to ultimately do with coroutines
+
     work.add_task([](run_queue* tasks) {
-            std::cout << "first task\n";
-            // first task adds a second one as a "callback"
-            tasks->add_task([](run_queue* tasks) {
-                    std::cout << "second task\n";
+            // future coroutine
+            int a = 2;
+            int b = 3;
+            int c = 4;
+            // queue long-running task
+            tasks->add_task([=](run_queue* tasks) {
+                    multiply(a, b,
+                             [c](int product) {
+                                 int result = product + c;
+                                 std::cout << "result: " << result << "\n";
+                             });
                 });
         });
     work.run();
