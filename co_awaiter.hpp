ca91122ae2747e51362bdd5328f9d053aa8bdf63
Jeff Trull <jeff.trull@ciere.com>
2018-05-18 10:02:25 -0700

    Move promise/return object for co_await demo into its own header
    Also add support for return_value() in non-void cases

---
+++ co_awaiter.hpp
@@ -0,0 +1,81 @@
+// A basic return object and promise type for a coroutine that co_awaits
+/*
+Copyright (c) 2018 Jeff Trull <edaskel@att.net
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef CO_AWAITER_HPP
+#define CO_AWAITER_HPP
+
+#include <type_traits>
+#include <experimental/coroutine>
+
+template<typename T=void>
+struct await_return_object {
+
+    // promise type must have either return_void or return_value member but not both
+    // not even if one is SFINAEd out - you cannot have both names present, per Lewis Baker
+    template<typename U=T>
+    struct promise_base {
+        auto return_value(U const&) const noexcept {
+            return std::experimental::suspend_always(); // ?? not sure
+        }
+    };
+    // void specialization to replace return_value() with return_void() is below at namespace scope
+
+    struct promise_type : promise_base<> {
+        // coroutine promise requirements:
+
+        auto initial_suspend() const noexcept {
+            return std::experimental::suspend_never(); // produce at least one value
+        }
+
+        auto final_suspend() const noexcept {
+            return std::experimental::suspend_always(); // ?? not sure
+        }
+
+        // either return_void or return_value will exist, depending on T,
+        // thanks to promise_base
+
+        await_return_object get_return_object() {
+            return await_return_object(*this);
+        }
+
+        void unhandled_exception() {}  // do nothing :)
+
+    };
+
+    await_return_object(promise_type & p) : m_coro(std::experimental::coroutine_handle<promise_type>::from_promise(p)) {}
+
+private:
+    std::experimental::coroutine_handle<promise_type> m_coro;
+
+};
+
+// specialization of promise_base to introduce return_void(), omit return_value()
+template<>
+template<>
+struct await_return_object<void>::promise_base<void> {
+    auto return_void() const noexcept {
+        return std::experimental::suspend_always(); // ?? not sure
+    }
+};
+
+#endif // CO_AWAITER_HPP
