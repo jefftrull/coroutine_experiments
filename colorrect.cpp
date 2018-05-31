23db008b73cf7459d5a89a66ee11c88e2d638888
Jeff Trull <jeff.trull@ciere.com>
2018-05-31 16:28:48 -0700

    Add a simple Qt example (without coroutines)

---
+++ colorrect.cpp
@@ -0,0 +1,60 @@
+// Implementation of a simple Qt colored rectangle widget, for experiments
+/*
+Copyright (c) 2018 Jeff Trull <edaskel@att.net>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "colorrect.h"
+
+ColorRect::ColorRect(QWidget *parent) :
+    QWidget{parent},
+    colorList{{"#111111", "#113311",
+                "#111133", "#331111",
+                "#333311", "#331133",
+                "#661111", "#116611",
+                "#111166", "#663311",
+                "#661133", "#336611",
+                "#331166", "#113366"}},
+    curColor{0},
+    changeTimer{new QTimer(this)}
+{
+    // set up the timer
+    connect(changeTimer, SIGNAL(timeout()),
+            this,        SLOT(changeColor()));
+    changeTimer->start(500);   // twice per second
+}
+
+void
+ColorRect::setColor(std::string const& col)
+{
+    setStyleSheet(("background-color:" + col).c_str());
+}
+
+void
+ColorRect::changeColor()
+{
+    curColor++;
+    if (curColor >= colorList.size())
+    {
+        curColor = 0;
+    }
+    setColor(colorList[curColor]);
+}
+
